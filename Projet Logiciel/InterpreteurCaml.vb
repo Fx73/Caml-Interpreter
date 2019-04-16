9b12cd6a0a8ce9c7539e55b35722bcaa81752074
fx <fx73000@yahoo.fr>
2019-04-16 21:38:50 +0200

    Arbres OK

--- Projet Logiciel/InterpreteurCaml.vb
+++ Projet Logiciel/InterpreteurCaml.vb
@@ -3,8 +3,10 @@ Imports System.IO
 
 #Region "Mes Classes de variables"
 Public Class Lexeme
-    Enum VarType
+    Enum LType
         Generic
+        Phrase
+        Expr
         Lint
         Lfloat
         Lmathop
@@ -17,7 +19,7 @@ Public Class Lexeme
     End Enum
 
     Public value As String
-    Public type As VarType
+    Public type As LType
     Public node As noeud
 
     Public Class noeud
@@ -25,7 +27,7 @@ Public Class Lexeme
         Public suiv As noeud
     End Class
 
-    Sub New(ByVal mavaleur As String, montype As VarType)
+    Sub New(ByVal mavaleur As String, montype As LType)
         value = mavaleur
         type = montype
     End Sub
@@ -83,16 +85,16 @@ Public Class Form1
 
     'Mes Chaines de stocks de Variables
     Public variables As VarPerso
-    Public phrase As Lexeme = New Lexeme("DEBUT", Lexeme.VarType.Lstring)
+    Public stock As VarPerso
+    Public phrase As Lexeme = New Lexeme("DEBUT", Lexeme.LType.Lstring)
     Public courant As Lexeme = phrase
     Public result
     'Ma Grammaire
-    Public GrammaireMots() As String = New String() {"(", ")", "let", "true", "false"}
+    Public GrammaireMots() As String = New String() {"let", "true", "false"}
 
     Enum ETAT As Int16
         PHRASE
         EXPR
-        CONS
         LINT
         LFLOAT
         LBOOL
@@ -104,7 +106,7 @@ Public Class Form1
     End Enum
 
     Public GrammaireMathOperateurs() As Char = New Char() {"+", "-", "*", "/", "<", ">", "="}
-    Public GrammaireLogicOperateurs() As Char = New Char() {"<", ">", "=", "&", "|"}
+    Public GrammaireLogicOperateurs() As Char = New Char() {"<", ">", "=", "!", "&", "|"}
 
 
 #Region "Description Fonctionnelle"
@@ -219,7 +221,7 @@ Public Class Form1
             Case ETAT.LBOOL
 
             Case ETAT.LVAR
-                If courant.type = Lexeme.VarType.Lvar Then
+                If courant.type = Lexeme.LType.Lvar Then
                     Return EmpileVar(courant.value)
                 Else
                     Erreur("NOM DE VARIABLE CORRECT ATTENDU")
@@ -227,107 +229,145 @@ Public Class Form1
         End Select
     End Function
 
-    Private Function Decoupage(ByRef s As String, l As Lexeme) As Lexeme.VarType
-        For i = 0 To s.Length - 1
-            'PARENTHESE
-            If s(i) = "(" Then
-                i += 1
-                Dim fin = Findparenthese(s.Substring(i))
-                Dim lex = AddLexeme(l, "()")
-                Dim atype = Decoupage(s.Substring(i, fin), lex)
-                lex.type = atype
-                i += fin + 1
-                'OPERATEURS MATHEMATIQUES
-            ElseIf EstMathOp(s(i)) Then
-                If OpePrio(s(i)) Then
-                    Dim lex = AddLexeme(l)
-                    Dim atype = Decoupage(s.Substring(0, i), lex)
-                    lex.type = atype
-                    AddLexeme(l, s(i), Lexeme.VarType.Lmathop)
-                    lex = AddLexeme(l)
-                    atype = Decoupage(s.Substring(0, i), lex)
-                    lex.type = atype
-                Else
 
+    Private Function Decoupage(ByRef s As String, l As Lexeme) As Boolean
+        'Decoupage en arbre par priorité
+        Dim searcher
+        Dim verif
+
+        'Supprimer les parentheses aux extremités
+        If (s(0) = "(" And s(s.Length - 1) = ")") Then
+            l.value = "()"
+            l.type = Lexeme.LType.Generic
+
+            Decoupage(s.Substring(1, s.Length - 2), AddLexeme(l))
+
+            Return True
+        End If
+
+
+        '0 Chercher les attributions "let"
+        searcher = Recherche("let ", s)
+        If searcher <> -1 Then
+            l.value = "let"
+            l.type = True
+
+            Dim i = searcher + 4
+            While i < s.Length - 1
+                If EstVar(s(i)) Then
+                    i += 1
+                Else
+                    Exit While
                 End If
+            End While
 
+            Decoupage(s.Substring(searcher + 4, i), AddLexeme(l))
 
-
-
-
-
-
-                'ENTIER et FLOAT
-            ElseIf EstInt(s(i)) Then
-                    Dim j = 1
-                    While (EstInt(s(i + j)))
-                        j += 1
-                    End While
-
-                    If (s(i + j) = ".") Then
-                        j += 1
-                        While (EstInt(s(i + j)))
-                            j += 1
-                        End While
-                        AddLexeme(s.Substring(i, j), Lexeme.VarType.Lfloat)
-
-                    ElseIf (s(i + j) = " " Or s(i + j) = ";" Or EstMathOp(s(i + j)) Or EstPar(s(i + j))) Then
-                        AddLexeme(s.Substring(i, j), Lexeme.VarType.Lint)
-
-                    ElseIf (EstVar(s(i + j))) Then
-                        While (EstVar(s(i + j)))
-                            j += 1
-                        End While
-                        AddLexeme(s.Substring(i, j), Lexeme.VarType.Lvar)
-
-                    Else
-                        Erreur("NUMBER ")
-                        Return False
-                    End If
-
-
-                ElseIf s(i) = "'" Then
-                    If s(i + 2) = "'" Then
-                        AddLexeme(s(i + 1), Lexeme.VarType.Lchar)
-                        i += 3
-                    Else
-                        Erreur("CHAR ")
-                        Return False
-                    End If
-                    'STRING
-                ElseIf s(i) = """" Then
-                    Dim j = 1
-                    While (s(i + j) <> """")
-                        If (i + j >= s.Length - 1) Then
-                            Erreur("STRING ")
-                            Return False
-                        End If
-                        j += 1
-                    End While
-                    AddLexeme(s.Substring(i + 1, j), Lexeme.VarType.Lstring)
-                    'NOM DE VAR
-                ElseIf EstVar(s(i)) Then
-                    Dim j = 1
-                    While (EstVar(s(i + j)))
-                        j += 1
-                    End While
-                    If EstMot(s.Substring(i, j)) Then
-                        AddLexeme(s.Substring(i, j), Lexeme.VarType.Lmot)
-                    Else
-                        AddLexeme(s.Substring(i, j), Lexeme.VarType.Lvar)
-                    End If
+            While i < s.Length - 1
+                If s(i) = " " Then
+                    i += 1
                 Else
-                    Erreur("DEBUT DE LEXEME INCONNU ")
+                    Exit While
+                End If
+            End While
+            If (s(i) <> "=") Then
+                Erreur("Mauvais operateur apres le let")
                 Return False
             End If
-        Next
-        Return True
-    End Function
 
+            Decoupage(s.Substring(i + 1), AddLexeme(l))
+
+            Return True
+        End If
+
+        '1 Chercher les séparateurs |
+        searcher = Recherche("|", s)
+        If searcher <> -1 Then
+            l.value = "|"
+            l.type = Lexeme.LType.Lbool
+            Decoupage(s.Substring(0, searcher), AddLexeme(l))
+            Decoupage(s.Substring(searcher + 1), AddLexeme(l))
+            Return True
+        End If
+
+        '2 Chercher les séparateurs &
+        searcher = Recherche("&", s)
+        If searcher <> -1 Then
+            l.value = "&"
+            l.type = Lexeme.LType.Lbool
+            Decoupage(s.Substring(0, searcher), AddLexeme(l))
+            Decoupage(s.Substring(searcher + 1), AddLexeme(l))
+            Return True
+        End If
+
+        '3 Chercher les séparateurs < > = !
+        searcher = Recherche(New Char() {"<", ">", "=", "!"}, s)
+        If searcher <> -1 Then
+            l.value = s(searcher)
+            l.type = Lexeme.LType.Lbool
+            Decoupage(s.Substring(0, searcher), AddLexeme(l))
+            Decoupage(s.Substring(searcher + 1), AddLexeme(l))
+            Return True
+        End If
+
+        '4 Chercher les séparateurs * /
+        searcher = Recherche(New Char() {"*", "/"}, s)
+        If searcher <> -1 Then
+            l.value = s(searcher)
+            l.type = Lexeme.LType.Expr
+            Decoupage(s.Substring(0, searcher), AddLexeme(l))
+            Decoupage(s.Substring(searcher + 1), AddLexeme(l))
+            Return True
+        End If
+
+        '5 Chercher les séparateurs + -
+        searcher = Recherche(New Char() {"+", "-"}, s)
+        If searcher <> -1 Then
+            l.value = s(searcher)
+            l.type = Lexeme.LType.Expr
+            Decoupage(s.Substring(0, searcher), AddLexeme(l))
+            Decoupage(s.Substring(searcher + 1), AddLexeme(l))
+            Return True
+        End If
+
+        'Identifier les opérandes
+        Select Case s(0)
+            Case "#"
+                l.value = Extract(s)
+                l.type = Lexeme.LType.Lstring
+                Return True
+            Case "$"
+                l.value = Extract(s)
+                l.type = Lexeme.LType.Lchar
+                Return True
+        End Select
+        If EstInt(s(0)) Then
+            If (s.IndexOf(".") = -1) Then
+                l.value = s
+                l.type = Lexeme.LType.Lint
+                Return True
+            Else
+                l.value = s
+                l.type = Lexeme.LType.Lfloat
+                Return True
+            End If
+        End If
+        If EstVar(s(0)) Then
+            l.value = s
+            l.type = Lexeme.LType.Lvar
+            Return True
+        End If
+
+        Erreur("Caractere inconnu : <<" + s(0) + ">>")
+        Return False
+    End Function
+    Private Function Predecoupage(s As String)
+
+    End Function
 #End Region
 
 #Region "Fonctions manipulation de variables"
-    Private Function EmpileVar(nom As String, Optional type As Lexeme.VarType = Nothing, Optional valeur As Object = Nothing)
+    Private Function EmpileVar(nom As String, Optional type As Lexeme.LType = Nothing, Optional valeur As Object = Nothing)
         Dim var = SearchVar(nom)
         If var Is Nothing Then
             var = variables
@@ -335,15 +375,15 @@ Public Class Form1
                 var = var.suiv
             End While
             Select Case type
-                Case Lexeme.VarType.Lint
+                Case Lexeme.LType.Lint
                     var.suiv = New Vint(nom, valeur)
-                Case Lexeme.VarType.Lfloat
+                Case Lexeme.LType.Lfloat
                     var.suiv = New Vfloat(nom, valeur)
-                Case Lexeme.VarType.Lbool
+                Case Lexeme.LType.Lbool
                     var.suiv = New Vbool(nom, valeur)
-                Case Lexeme.VarType.Lchar
+                Case Lexeme.LType.Lchar
                     var.suiv = New Vchar(nom, valeur)
-                Case Lexeme.VarType.Lstring
+                Case Lexeme.LType.Lstring
                     var.suiv = New Vstring(nom, valeur)
                 Case Else
                     var.suiv = New VarPerso(nom, valeur)
@@ -353,7 +393,7 @@ Public Class Form1
         Return var
     End Function
 
-    Private Function AddLexeme(root As Lexeme, Optional valeur As String = Nothing, Optional type As Lexeme.VarType = Lexeme.VarType.Generic) As Lexeme
+    Private Function AddLexeme(root As Lexeme, Optional valeur As String = Nothing, Optional type As Lexeme.LType = Lexeme.LType.Generic) As Lexeme
         Dim l = NewNode(root)
         l.lex = New Lexeme(valeur, type)
         Return l.lex
@@ -376,9 +416,21 @@ Public Class Form1
     Private Function Avancer()
     End Function
 
+    Private Function Extract(s As String)
+        Dim index As VarPerso = stock
+        While index.suiv IsNot Nothing
+            If index.suiv.nom = s Then
+                Dim temp = index.suiv
+                index.suiv = index.suiv.suiv
+                Return temp.value
+            End If
+            index = index.suiv
+        End While
+        Return Nothing
+    End Function
+
     Private Function SearchVar(s As String)
-        Dim index As VarPerso
-        index = Variables
+        Dim index As VarPerso = variables
         While index IsNot Nothing
             If index.nom = s Then
                 Return index
@@ -392,6 +444,43 @@ Public Class Form1
 #End Region
 
 #Region "Fonctions utilitaires"
+    Private Function Recherche(mot As String, s As String)
+        Dim first = s.IndexOf(mot)
+        While (first <> -1 AndAlso IsInParenthese(first, s))
+            s.Substring(first + 1).IndexOf(mot)
+        End While
+        Return first
+    End Function
+    Private Function Recherche(mot As Char(), s As String)
+        Dim first = s.IndexOfAny(mot)
+        While (first <> -1 AndAlso IsInParenthese(first, s))
+            s.Substring(first + 1).IndexOfAny(mot)
+        End While
+        Return first
+    End Function
+
+    Private Function IsInParenthese(index As Integer, s As String) As Boolean
+        Dim compt = 0
+
+        For i = 0 To index
+            If s(i) = "(" Then
+                compt += 1
+            End If
+            If s(i) = ")" Then
+                compt -= 1
+            End If
+        Next
+
+        If compt < 0 Then
+            Erreur("Parenthese fermante de trop")
+            Return True
+        End If
+
+        Return compt = 0
+    End Function
+
+
+
     Private Function Findparenthese(s As String) As Integer
         Dim compt = 1
         For i = 0 To s.Length - 1
@@ -410,7 +499,20 @@ Public Class Form1
     End Function
 
     Private Function Findopeprio(s As String) As Integer
-        For i = 0 To s.Length - 1
+        Dim lim = s.Length
+        For Each mot In (GrammaireMots)
+            Dim a = s.IndexOf(mot)
+            If (a <> -1 And lim > a) Then
+                lim = a
+            End If
+        Next
+        For Each mot In (GrammaireLogicOperateurs)
+            Dim a = s.IndexOf(mot)
+            If (a <> -1 And lim > a) Then
+                lim = a
+            End If
+        Next
+        For i = 0 To lim - 1
             If OpePrio(s(i)) Then
                 Return i
             End If
@@ -418,9 +520,10 @@ Public Class Form1
                 i += Findparenthese(s.Substring(i + 1))
             End If
             If s(i) = "=" Then
-
+                Return 0
             End If
         Next
+        Return 0
     End Function
 
     Private Function EstChar(s As Char) As Boolean
